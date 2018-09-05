fcf7aee10c386de7d7ad802ed7820d58561d6b1f
droldrl92 <droldrl92@gmail.com>
2018-09-04 23:53:51 -0500

    etapa avanzada notas

---
+++ src/data/data.notas.ts
@@ -0,0 +1,68 @@
+export const Alta = [
+    {aceite: "Bergamota"},
+    {aceite: "Abedul"},
+    {aceite: "Cilantro"},
+    {aceite: "Toronja"},
+    {aceite: "Limón"},
+    {aceite: "Hierba Limonera"},
+    {aceite: "Lima"},
+    {aceite: "Naranja Silvestre"},
+    {aceite: "Hierbabuena"},
+    {aceite: "Mandarina"},
+    {aceite: "Gaulteria"},
+    {aceite: "Petitgrain"}
+];
+
+export const MediaAlta = [
+    {aceite: "Arborvitae/Tuya"},
+    {aceite: "Albahaca"},
+    {aceite: "Hinojo Dulce"}
+];
+
+export const Media = [
+    {aceite: "Pimienta Negra"},
+    {aceite: "Cardamomo"},
+    {aceite: "Casia"},
+    {aceite: "Cedro"},
+    {aceite: "Canela"},
+    {aceite: "Cilantro"},
+    {aceite: "Ciprés"},
+    {aceite: "Eneldo"},
+    {aceite: "Eucalipto"},
+    {aceite: "Geranio"},
+    {aceite: "Jengibre"},
+    {aceite: "Helicriso"},
+    {aceite: "Enebro"},
+    {aceite: "Lavanda"},
+    {aceite: "Mejorana"},
+    {aceite: "Melaleuca"},
+    {aceite: "Melisa"},
+    {aceite: "Orégano"},
+    {aceite: "Menta"},
+    {aceite: "Manzanilla Romana"},
+    {aceite: "Romero"},
+    {aceite: "Tomillo"},
+    {aceite: "Abeto Blanco"},
+    {aceite: "Baya de Enebro"}
+];
+
+
+export const Base = [
+    {aceite: "Incienso"},
+    {aceite: "Jazmín"},
+    {aceite: "Mirra"},
+    {aceite: "Pachuli"},
+    {aceite: "Sándalo Hawaiano"},
+    {aceite: "Vetiver"},
+    {aceite: "Nardo"},
+    {aceite: "Ravensara"}
+];
+
+
+
+export const MediaBase = [
+    {aceite: "Salvia Esclarea"},
+    {aceite: "Clavo de Olor"},
+    {aceite: "Rosa"},
+    {aceite: "Ylang Ylang"}
+];
--- src/pages/notas/notas.ts
+++ src/pages/notas/notas.ts
@@ -1,15 +1,296 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { Alta, MediaAlta, Media, MediaBase, Base } from "../../data/data.notas";
+import { Aceite } from "../../interfaces/aceite.interface";
 
 @Component({
   selector: 'page-notas',
   templateUrl: 'notas.html',
 })
 export class NotasPage {
+  altas:Aceite[] = [];
+  mediasaltas:Aceite[] = [];
+  medias:Aceite[] = [];
+  mediasbases:Aceite[] = [];
+  bases:Aceite[]=[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  alta:number = 0;
+  mediaalta:number = 0;
+  media:number = 0;
+  mediabase:number = 0;
+  base:number = 0;
+
+  auxmediabase:number = 0;
+  auxmedia:number = 0;
+  auxmediaalta:number = 0;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+    this.altas = Alta;
+    this.mediasaltas = MediaAlta;
+    this.medias = Media;
+    this.mediasbases = MediaBase;
+    this.bases = Base;
   }
 
+
+
+
+  Alta() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Elige los aceites de nota Alta');
+
+    for (let key in this.altas) {
+      alert.addInput({
+        type: 'checkbox',
+        label: this.altas[key].aceite,
+        value: this.altas[key].aceite
+      });
+    }
+
+
+    alert.addButton({
+      text: 'Aceptar',
+      handler: data => {
+        console.log('Checkbox data Alta:', data);
+        if (data.length > 2) {
+            let alert = this.alertCtrl.create({
+              title: 'Cantidad de Aceites',
+              subTitle: 'Se recomienda solo ingresar máximo 2 aceites.',
+              buttons: ['OK']
+            });
+            alert.present();
+            return false;
+        }
+        else
+        {
+          if (data.length >= 1) {
+              this.alta = Math.ceil((0.10/data.length)*16);
+          }
+        }
+      }
+    });
+    alert.addButton('Cancelar');
+    alert.addButton({
+      text: 'Reiniciar',
+      handler: data => {
+        console.log('Checkbox data Alta:', data);
+        this.alta = 0;
+      }
+    });
+    alert.present().then(() => {
+
+    });
+  }
+
+  Base() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Elige los aceites de nota Base');
+
+    for (let key in this.bases) {
+      alert.addInput({
+        type: 'checkbox',
+        label: this.bases[key].aceite,
+        value: this.bases[key].aceite
+      });
+    }
+
+    alert.addButton({
+      text: 'Aceptar',
+      handler: data => {
+        console.log('Checkbox data Base:', data);
+        if (data.length > 2) {
+            let alert = this.alertCtrl.create({
+              title: 'Cantidad de Aceites',
+              subTitle: 'Se recomienda solo ingresar máximo 2 aceites.',
+              buttons: ['OK']
+            });
+            alert.present();
+            return false;
+        }
+        else
+        {
+          if (data.length >= 1) {
+              this.base = Math.ceil((0.10/data.length)*16);
+          }
+        }
+      }
+    });
+
+    alert.addButton('Cancelar');
+
+    alert.addButton({
+      text: 'Reiniciar',
+      handler: data => {
+        console.log('Checkbox data base:', data);
+        this.base = 0;
+      }
+    });
+
+    alert.present().then(() => {
+
+    });
+  }
+
+  Media() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Elige los aceites de nota Base');
+
+    for (let key in this.medias) {
+      alert.addInput({
+        type: 'checkbox',
+        label: this.medias[key].aceite,
+        value: this.medias[key].aceite
+      });
+    }
+
+
+    alert.addButton({
+      text: 'Aceptar',
+      handler: data => {
+        console.log('Checkbox data Media:', data);
+        if (data.length > 4) {
+            let alert = this.alertCtrl.create({
+              title: 'Cantidad de Aceites',
+              subTitle: 'Se recomienda solo ingresar máximo 4 aceites.',
+              buttons: ['OK']
+            });
+            alert.present();
+            return false;
+        }
+        else
+        {
+          if (data.length >= 1) {
+            this.auxmedia = data.length;
+
+              this.media = Math.ceil((0.25/data.length)*16);
+          }
+        }
+      }
+    });
+    alert.addButton('Cancelar');
+
+    alert.addButton({
+      text: 'Reiniciar',
+      handler: data => {
+        console.log('Checkbox data Alta:', data);
+        this.alta = 0;
+      }
+    });
+
+    alert.present().then(() => {
+
+    });
+  }
+
+  MediaAlta() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Elige los aceites de nota Media-Alta');
+
+    for (let key in this.mediasaltas) {
+      alert.addInput({
+        type: 'checkbox',
+        label: this.mediasaltas[key].aceite,
+        value: this.mediasaltas[key].aceite
+      });
+    }
+
+
+    alert.addButton({
+      text: 'Aceptar',
+      handler: data => {
+        console.log('Checkbox data Media-Alta:', data);
+        if (data.length > 4) {
+            let alert = this.alertCtrl.create({
+              title: 'Cantidad de Aceites',
+              subTitle: 'Se recomienda solo ingresar máximo 4 aceites.',
+              buttons: ['OK']
+            });
+            alert.present();
+            return false;
+        }
+        else
+        {
+          if (data.length >= 1) {
+            this.auxmedia = data.length;
+
+              this.mediaalta = Math.ceil((0.25/data.length)*16);
+          }
+        }
+      }
+    });
+    alert.addButton('Cancelar');
+
+    alert.addButton({
+      text: 'Reiniciar',
+      handler: data => {
+        console.log('Checkbox data Alta:', data);
+        this.mediaalta = 0;
+      }
+    });
+
+    alert.present().then(() => {
+
+    });
+  }
+
+  MediaBase() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Elige los aceites de nota Media-Base');
+
+    for (let key in this.mediasbases) {
+      alert.addInput({
+        type: 'checkbox',
+        label: this.mediasbases[key].aceite,
+        value: this.mediasbases[key].aceite
+      });
+    }
+
+
+    alert.addButton({
+      text: 'Aceptar',
+      handler: data => {
+        console.log('Checkbox data Media:', data);
+        if (data.length > 4) {
+            let alert = this.alertCtrl.create({
+              title: 'Cantidad de Aceites',
+              subTitle: 'Se recomienda solo ingresar máximo 4 aceites.',
+              buttons: ['OK']
+            });
+            alert.present();
+            return false;
+        }
+        else
+        {
+          if (data.length >= 1) {
+            this.auxmedia = data.length;
+
+              this.mediabase = Math.ceil((0.25/data.length)*16);
+          }
+        }
+      }
+    });
+    alert.addButton('Cancelar');
+
+    alert.addButton({
+      text: 'Reiniciar',
+      handler: data => {
+        console.log('Checkbox data MediBase:', data);
+        this.mediabase = 0;
+      }
+    });
+
+    alert.present().then(() => {
+
+    });
+  }
+
+  Reiniciar(){
+    this.alta = 0;
+    this.mediaalta = 0;
+    this.media = 0;
+    this.mediabase = 0;
+    this.base = 0;
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad NotasPage');
   }
